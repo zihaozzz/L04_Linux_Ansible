d3c7824251a4946f8780d4a2223e7ba05ce7ffe9
William <william@13900K.com>
2023-06-03 22:21:38 +1000

    variables

--- Linux/variables.sh
+++ Linux/variables.sh
@@ -1,75 +1,75 @@
 #!/usr/bin/env bash
 
-# # scriptName=$0
+scriptName=$0
 
-# # echo "value of varible in position 0 is $scriptName"
+echo "value of varible in position 0 is $scriptName"
 
-# name=$1
+name=$1
 
-# echo "my name is $name"
+echo "my name is $name"
 
 
 
-# echo $name
+echo $name
+# Right
+echo "Welcome" $name
+# Right
+echo "Welcome ${name}"
+# Wrong
+echo 'Welcome ${name}'
+#Wrong
+echo 'Welcome (name)'
 
-# echo "Welcome" $name
+read name
 
-# echo "Welcome ${name}"
-
-# echo 'Welcome ${name}'
-
-# echo 'Welcome (name)'
-
-# read name
-
-# echo "Welcome $name"
+echo "Welcome $name"
 
 # execute command
 
-# echo $(ls)
+echo $(ls)
 
 
-# # +, - , * , /
+# +, - , * , /
 
-# read X
-# read Y
+read X
+read Y
 
 # X+Y
 # X-Y
 # X*Y
 # X/Y
 
-# echo $((5+3))
-# echo $((X+Y))
-# echo $((X-Y))
-# echo $((X*Y))
-# echo $((X/Y))
+echo $((5+3))
+echo $((X+Y))
+echo $((X-Y))
+echo $((X*Y))
+echo $((X/Y))
 
 
-# read X
-# read Y
-# [[ $X -gt $Y ]] && echo "$X is greater than $Y"
-# [[ $X -eq $Y ]] && echo "$X is equal to $Y"
-# [[ $X -lt $Y ]] && echo "$X is less than $Y"
+read X
+read Y
+[[ $X -gt $Y ]] && echo "$X is greater than $Y"
+[[ $X -eq $Y ]] && echo "$X is equal to $Y"
+[[ $X -lt $Y ]] && echo "$X is less than $Y"
 
-# if (( $X > $Y )); then
-#     printf "$X is greater than $Y"
-# elif (( $X == $Y )); then
-#     printf "$X is equal to $Y"
-# else
-#     printf "$X is less than $Y"
-# fi
+if (( $X > $Y )); then
+    printf "$X is greater than $Y"
+elif (( $X == $Y )); then
+    printf "$X is equal to $Y"
+else
+    printf "$X is less than $Y"
+fi
 
 
-# # Anything inside single parentheses is considered as command and executed as separate subprocess.
-# read input
-# # || means OR in condition && means AND condition
-# if [ $input = "Y" ] && [ $input = "y" ]; then
-# # if (($input > 0)) || (($input < 10 )); then
-#     echo YES
-# else
-#     echo NO
-# fi
+# Anything inside single parentheses is considered as command and executed as separate subprocess.
+read input
+# || means OR in condition && means AND condition
+if [ $input = "Y" ] || [ $input = "y" ]; then
+# if (($input > 0)) && (($input < 10 )); then
+    echo YES
+else
+    echo NO
+fi
 
 read a
 read b
@@ -80,7 +80,7 @@ if [ $a -eq $b ] || [ $a -eq $c ] || [ $c -eq $b ];then
         echo "EQUILATERAL"
     else
         echo "ISOSCELES"
-        fi
+    fi
 else
     echo "SCALENE"
-    fi
+fi
